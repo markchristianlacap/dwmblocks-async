b43ae86f3d89fee7f0990d26d2c8ce181086a7a1
Mark Christian Lacap <markchristianlacap@gmail.com>
2025-11-28 14:41:23 +0800

    feat: add bluetoot connected

--- config.h
+++ config.h
@@ -17,12 +17,17 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)             \
-    X("", "amixer get Master | awk -F'[][]' 'END{if($4==\"off\"){print \"MUTE\"}else{print \"VOL \"$2}}'", 1, 0)  \
-    X("CPU", "top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8 \"%\"}'", 1, 0)   \
-    X("TEMP", "sensors | awk '/Package id 0:/ {print $4}'", 5, 0) \
-    X("MEM", "free -h --giga | awk '/Mem:/ {print $3 \"/\" $2}'", 5, 0) \
-    X("IP", "ip -4 addr show wlo1 | awk '/inet / {print $2}' | cut -d/ -f1", 10, 0) \
-    X("", "date '+%a %b %d, %Y %H:%M'", 30, 0)     \
+#define BLOCKS(X)                                                             \
+    X("", "~/.scripts/bluetooth_status", 1, 0)                                \
+    X("",                                                                     \
+      "amixer get Master | awk -F'[][]' 'END{if($4==\"off\"){print "          \
+      "\"MUTE\"}else{print \"VOL \"$2}}'",                                    \
+      1, 0)                                                                   \
+    X("CPU", "top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8 \"%\"}'", 1, 0) \
+    X("TEMP", "sensors | awk '/Package id 0:/ {print $4}'", 5, 0)             \
+    X("MEM", "free -h --giga | awk '/Mem:/ {print $3 \"/\" $2}'", 5, 0)       \
+    X("IP", "ip -4 addr show wlan0 | awk '/inet / {print $2}' | cut -d/ -f1", \
+      10, 0)                                                                  \
+    X("", "date '+%a %b %d, %Y %H:%M'", 1, 0)
 
 #endif  // CONFIG_H
